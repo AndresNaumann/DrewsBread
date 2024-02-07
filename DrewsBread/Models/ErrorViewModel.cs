911b9ee53a3b3c9ca5c60c130324612e944ed1f2
Drew Naumann <naumannadn@gmail.com>
2024-02-07 12:13:54 -0700

    Add project files.

---
+++ DrewsBread/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DrewsBread.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
